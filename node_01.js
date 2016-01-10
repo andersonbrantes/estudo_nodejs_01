e85f233b7b8f5d7325f8aa199cc12ee24d1831bb
AndersonBrantes <andersonbrantes@gmail.com>
2016-01-10 00:37:01 -0200

    organizando os request so server

--- node_01.js
+++ node_01.js
@@ -1,16 +1,28 @@
 
 var http = require('http');
 var fs = require('fs');
+var server = http.createServer();
 
-http.createServer(function(request, response) {
+server.on('request', function(request, response) {
   response.writeHead(200, {
     'Content-Type': 'text/html'
   });
+  response.write('Hello, this is novoUsuario');
+  response.end();
 
-  fs.readFile('index.html', function(err, contents) {
-    response.write(contents);
-    response.end();
-  });
+//  fs.readFile('index.html', function(err, contents) {
+//    response.write(contents);
+//    response.end();
+//  });
+});
 
-}).listen(8080);
+server.on('request', function(){
+  console.log('New request coming in...');
+});
+
+server.on('close', function(){
+  console.log('Closing down the server...');
+});
+
+server.listen(8080);
 
