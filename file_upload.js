36df0ea792e43f9dff898d1cdd5f099e51a80097
AndersonBrantes <andersonbrantes@gmail.com>
2016-01-13 23:08:55 -0200

    adicionando express e adicionando primeiro exercicio

--- file_upload.js
+++ file_upload.js
@@ -1,11 +1,19 @@
 var fs = require('fs');
+var http = require('http');
 
-var file = fs.createReadStream('fruits.txt');
+http.createServer(function(request, response){
+  var newfile = fs.createWriteStream('new_file.txt');
+  var fileBytes = request.headers['content-length'];
+  var uploadedBytes = 0;
 
-file.on('readable', function(){
-  var chunk;
+  request.on('readable', function(){
+    var chunk = null;
+    while(null !== (chunk = request.read())){
+      uploadedBytes += chunk.length;
+      var progress = (uploadedBytes / fileBytes) * 100;
+      response.write('progress' + parseInt(progress, 10) + '%\n');
+    }
+  });
 
-  while(null !== (chunk = file.read())){
-    console.log(chunk.toString());
-  }
-});
+  request.pipe(newfile);
+}).listen(8080);
